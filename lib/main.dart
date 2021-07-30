7dfd4bb00fa73da64ffad31cdd89bc2a31c6dc31
adityapuri75 <adityapuri75@gmail.com>
2021-07-30 20:15:28 +0530

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:camhed/Client/Pages/splashScreen/SplashScreen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
