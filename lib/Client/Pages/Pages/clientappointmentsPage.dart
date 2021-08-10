0ecef46ae0a1263aaa3443af9df4dbaac45f8323
adityapuri75 <adityapuri75@gmail.com>
2021-08-11 00:41:38 +0530

    Client Appointments page done

--- lib/Client/Pages/Pages/clientappointmentsPage.dart
+++ lib/Client/Pages/Pages/clientappointmentsPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ticket_widget/flutter_ticket_widget.dart';
 
@@ -15,6 +16,7 @@ class _ClientAppointmentPageState extends State<ClientAppointmentPage> {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
           "Appointments",
@@ -27,125 +29,229 @@ class _ClientAppointmentPageState extends State<ClientAppointmentPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 15,right: 15,top: 20),
-              child: FlutterTicketWidget(width: width,
-                  height: height/3,
-                  color: Colors.black12,
-                  isCornerRounded: true,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10,left: 5,right: 5),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 7,
-                                  top: 7,
-                                  right: 7,
-                                  bottom: 5),
-                              child: Row(
-                                crossAxisAlignment:
-                                CrossAxisAlignment.center,
-                                mainAxisAlignment:
-                                MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
+            ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+         scrollDirection: Axis.vertical,
+      itemCount: 5,
+       itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 15,right: 15,top: 20),
+                child: Material(
+                  elevation: 2,
+                  borderRadius: BorderRadius.circular(height/80),
+                  child: Container(width: width,
+                      height: height/3.7,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(height/80),
+                        color: Colors.white,
+                      ),
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 1,left: 5,right: 5),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 7,
+                                      top: 1,
+                                      right: 7,
+                                      bottom: 5),
+                                  child: Row(
+                                    crossAxisAlignment:
+                                    CrossAxisAlignment.center,
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Container(
-                                        height: height / 10,
-                                        width: height / 10,
-                                        decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                                image: AssetImage(
-                                                    "Images/doctorlogo.png"))),
-                                      ),
-                                      SizedBox(
-                                        width: width / 25,
-                                      ),
-                                      Column(
-                                        crossAxisAlignment:
-                                        CrossAxisAlignment
-                                            .start,
+                                      Row(
                                         children: [
-                                          Text(
-                                            "Dr. Aditya",
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontWeight:
-                                                FontWeight.w600,
-                                                fontSize:
-                                                height / 55),
+                                          Container(
+                                            height: height / 10,
+                                            width: height / 10,
+                                            decoration: BoxDecoration(
+                                                image: DecorationImage(
+                                                    image: AssetImage(
+                                                        "Images/doctorlogo.png"))),
                                           ),
                                           SizedBox(
-                                            height: height / 250,
+                                            width: width / 25,
                                           ),
-                                          Text(
-                                            "Covid-19",
-                                            style: TextStyle(
-                                                color:
-                                                Colors.black38,
-                                                fontSize:
-                                                height / 65),
-                                          ),
-                                          SizedBox(
-                                            height: height / 200,
-                                          ),
-                                          Text(
-                                            "10:00 AM - 07:00 PM",
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontWeight:
-                                                FontWeight.w500,
-                                                fontSize:
-                                                height / 65),
-                                          ),
-                                          SizedBox(
-                                              height: height / 250),
-                                          Row(
+                                          Column(
+                                            crossAxisAlignment:
+                                            CrossAxisAlignment
+                                                .start,
                                             children: [
                                               Text(
-                                                "Time Status: ",
+                                                "Dr. Aditya",
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                    FontWeight.w600,
+                                                    fontSize:
+                                                    height / 55),
+                                              ),
+                                              SizedBox(
+                                                height: height / 250,
+                                              ),
+                                              Text(
+                                                "Covid-19",
                                                 style: TextStyle(
                                                     color:
                                                     Colors.black38,
                                                     fontSize:
                                                     height / 65),
                                               ),
+                                              SizedBox(
+                                                height: height / 200,
+                                              ),
                                               Text(
-                                                "Pending",
+                                                "10:00 AM - 07:00 PM",
                                                 style: TextStyle(
-                                                    color:
-                                                    Colors.orangeAccent,
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                    FontWeight.w500,
                                                     fontSize:
                                                     height / 65),
                                               ),
+                                              SizedBox(
+                                                  height: height / 250),
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                    "Time Status: ",
+                                                    style: TextStyle(
+                                                        color:
+                                                        Colors.black38,
+                                                        fontSize:
+                                                        height / 65),
+                                                  ),
+                                                  Text(
+                                                    "Pending",
+                                                    style: TextStyle(
+                                                        color:
+                                                        Colors.orangeAccent,
+                                                        fontSize:
+                                                        height / 65,fontWeight: FontWeight.w600),
+                                                  ),
+                                                ],
+                                              ),
                                             ],
-                                          ),
+                                          )
                                         ],
+                                      ),
+                                      Container(
+                                        height: height/15,
+                                      width: width/4,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage("Images/unpaid.jpg")
+                                        )
+                                      ),
                                       )
                                     ],
                                   ),
-                                  Container(
-                                  //  ////////////////////////////////////////
-                                  )
-                                ],
-                              ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 1),
+                                  child: Center(child: Divider()),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10,left: 15),
+                                  child: Row(
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            "MON",
+                                            style: TextStyle(
+                                              color: Colors.red,
+                                            ),
+                                          ),
+                                          Text(
+                                            "26",
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: height / 50,
+                                                fontWeight:
+                                                FontWeight.w500),
+                                          ),
+                                          Text(
+                                            "JUL",
+                                            style: TextStyle(
+                                              color: Colors.black38,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 15),
+                                        child: Container(height: height/15,width: 1,
+                                        color: Colors.black38,),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 10),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Text("Booking Status: "),
+                                                Text("Pending",style: TextStyle(color: Colors.orangeAccent,fontWeight: FontWeight.w600),)
+                                              ],
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(top: 3),
+                                              child: Row(
+                                                children: [
+                                                  Text("Appointment Id: ",style: TextStyle(color: Colors.black38),),
+                                                  Text("CamHed006235",style: TextStyle(color: Colors.black38),),
+                                                ],
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(top: 5),
+                                              child: Row(
+                                                children: [
+                                                  Text("Amount: ",style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontWeight:
+                                                  FontWeight.w500,
+                                                  fontSize:
+                                                  height / 55),),
+                                                  Text("\$ 55",style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontWeight:
+                                                      FontWeight.w500,
+                                                      fontSize:
+                                                      height / 55),),
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+
+                              ],
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 25),
-                              child: Center(child: Divider()),
-                            )
+                          ),
+                        ],
 
-                          ],
-                        ),
-                      ),
-                    ],
-
-                  )
-              ),
+                      )
+                  ),
+                ),
+              );}
             )
+
           ],
         ),
       ),
@@ -155,3 +261,11 @@ class _ClientAppointmentPageState extends State<ClientAppointmentPage> {
       );
   }
 }
+
+
+
+
+
+
+
+
