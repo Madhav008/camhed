5ecd96ea6ba144caea9c3362568784b7112070e1
Hari Om <58305594+Madhav008@users.noreply.github.com>
2021-08-22 01:17:41 +0530

    error removed

--- android/app/src/main/kotlin/com/zirkon/camhed/MainActivity.kt
+++ android/app/src/main/kotlin/com/zirkon/camhed/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zirkon.camhed
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterActivity() {
 }
